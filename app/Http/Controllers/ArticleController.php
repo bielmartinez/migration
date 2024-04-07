e64d57b715d451034a625d874a41a8e80ac331e0
bielmartinez <bielmartinezcaceres16@gmail.com>
2024-04-08 01:15:39 +0200

    funcions i estils

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,7 +8,16 @@ use App\Models\Article;
 class ArticleController extends Controller
 {
     public function index(){
-        $articles = Article::all();
-        return view('welcome');
+        $articles = Article::latest()->paginate(10);
+        return view('dashboard',compact('articles'));
+    }
+
+    public function welcome(){
+        $articles = Article::latest()->paginate(5);
+        return view('welcome',compact('articles'));
+    }
+
+    public function store(Request $request) {
+        dd($request);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,24 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+        <h2 class="font-bold text-2xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Artículos') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+    <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-600">
+        @forelse($articles as $article)
+        <li class="py-4">
+            <div class="bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/20 to-transparent dark:ring-1 dark:ring-inset dark:ring-white/10 rounded-lg shadow-md dark:shadow-none transition duration-300 ease-in-out hover:shadow-lg">
+                <div class="p-6">
+                    <p class="text-lg font-semibold text-gray-800 dark:text-gray-200">{{ $article->article }}</p>
                 </div>
             </div>
-        </div>
+        </li>
+        @empty
+        <li>No hi ha més articles.</li>
+        @endforelse
+    </ul>
+    <div class="mt-8">
+        {{ $articles->links() }}
     </div>
 </x-app-layout>
