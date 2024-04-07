daae26d7a158a1b9ac5667822ad2abb23f71fcab
bielmartinez <bielmartinezcaceres16@gmail.com>
2024-04-07 21:39:51 +0200

    Model Article

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
+    public function index(){
+        $articles = articles::all()->paginate(2);
+        return view('welcome');
+    }
 }
